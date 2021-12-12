2cd58ec4044e27511d9cebaf6f63676011c4770a
Yuneider <yparada22@gmail.com>
2021-12-11 22:01:34 -0500

    Icons Added to nav bar

--- js/hoverAside.js
+++ js/hoverAside.js
@@ -4,7 +4,12 @@ document.addEventListener('DOMContentLoaded', function () {
     aside.addEventListener('mouseenter',Enter);
     aside.addEventListener('mouseleave',Leave);
     
-    var container = document.getElementById("container").style;
+    const container = document.getElementById("container").style;
+    const home = document.getElementById("home").style;
+    const catalog = document.getElementById("catalog").style;
+    const logo = document.getElementById("logo").style;
+    const aboutUs = document.getElementById("aboutUs").style;
+    const menuContainer = document.getElementById("menuContainer").style;
 
     const e = "6vw";
     const l = "3vw";
@@ -13,12 +18,22 @@ document.addEventListener('DOMContentLoaded', function () {
     function Enter() {
         container.marginLeft = e;
         aside.style.width = e;
+        home.width = e;
+        catalog.width = e;
+        logo.width = e;
+        aboutUs.width = e;
+        menuContainer.top = "28vh";
         document.documentElement.style.setProperty('--margen',e);
     }
 
     function Leave() {
         container.marginLeft = l; 
         aside.style.width = l;
+        home.width = l;
+        catalog.width = l;
+        logo.width = l;
+        aboutUs.width = l;
+        menuContainer.top = "40vh";
         document.documentElement.style.setProperty('--margen',l);
     }
 
